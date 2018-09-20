9cf116b7b90cf4c6f87298b72f2a04434c24b755
Huanghuang <18778335525@163.com>
2018-09-20 16:56:53 +0800

    数组-使用泛型

--- Array/src/Array.java
+++ Array/src/Array.java
@@ -1,5 +1,5 @@
-public class Array {
-    private int[] data;
+public class Array<E> {
+    private E[] data;
     private int size;
 
     /**
@@ -7,7 +7,7 @@ public class Array {
      * @param capacity
      */
     public Array(int capacity){
-        data = new int[capacity];
+        data = (E[]) new Object[capacity];
         size = 0;
     }
 
@@ -32,17 +32,17 @@ public class Array {
     }
 
     // 向所有元素后添加一个新元素
-    public void addLast(int e){
+    public void addLast(E e){
         add(size, e);
     }
 
     // 向所有元素前添加一个新元素
-    public void addFirst(int e){
+    public void addFirst(E e){
         add(0, e);
     }
 
     // 在第index个位置插入一个新元素e
-    public void add(int index, int e){
+    public void add(int index, E e){
         if (size == data.length){
             throw new IllegalArgumentException("Add Failed.Array is full");
         }
@@ -61,7 +61,7 @@ public class Array {
     }
 
     // 获取index索引位置的元素
-    int get(int index){
+    E get(int index){
         if (index < 0 || index >= size){
             throw new IllegalArgumentException("Get Failed.Index is illegal");
         }
@@ -70,7 +70,7 @@ public class Array {
 
 
     // 设置index索引位置的元素
-    void set(int index, int e){
+    void set(int index, E e){
         if (index < 0 || index >= size){
             throw new IllegalArgumentException("Get Failed.Index is illegal");
         }
@@ -78,9 +78,9 @@ public class Array {
     }
 
     // 查找数组中是否有元素e
-    public boolean contains(int e){
+    public boolean contains(E e){
         for (int i = 0; i < size; i++) {
-            if(data[i] == e){
+            if(data[i].equals(e)){
                 return true;
             }
         }
@@ -88,9 +88,9 @@ public class Array {
     }
 
     // 查找数组中元素e的索引
-    public int find(int e){
+    public int find(E e){
         for (int i = 0; i < size; i++) {
-            if(data[i] == e){
+            if(data[i].equals(e)){
                 return i;
             }
         }
@@ -99,30 +99,31 @@ public class Array {
 
 
     // 从数组中删除index位置的元素，返回删除的元素
-    public int remove(int index){
+    public E remove(int index){
         if (index < 0 || index >= size){
             throw new IllegalArgumentException("Remove failed.Index is illegal");
         }
-        int ret = data[index];
+        E ret = data[index];
         for (int i = index + 1; i < size; i++){
             data[i-1] = data[i];
         }
         size --;
+        data[size] = null;
         return ret;
     }
 
     // 删除第一个元素
-    public int removeFirst(){
+    public E removeFirst(){
         return remove(0);
     }
 
     // 删除最后一个元素
-    public int removeLast(){
+    public E removeLast(){
         return remove(size - 1);
     }
 
     // 删除指定的元素
-    public void removeElement(int e){
+    public void removeElement(E e){
         int index = find(e);
         if(index != -1){
             remove(index);
