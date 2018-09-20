b0ba3858f33e5125808395e73323ef69c08a4d7c
Huanghuang <18778335525@163.com>
2018-09-20 15:49:31 +0800

    数组-搜索和删除数组元素

--- Array/src/Array.java
+++ Array/src/Array.java
@@ -77,6 +77,57 @@ public class Array {
         data[index] = e;
     }
 
+    // 查找数组中是否有元素e
+    public boolean contains(int e){
+        for (int i = 0; i < size; i++) {
+            if(data[i] == e){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 查找数组中元素e的索引
+    public int find(int e){
+        for (int i = 0; i < size; i++) {
+            if(data[i] == e){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    // 从数组中删除index位置的元素，返回删除的元素
+    public int remove(int index){
+        if (index < 0 || index >= size){
+            throw new IllegalArgumentException("Remove failed.Index is illegal");
+        }
+        int ret = data[index];
+        for (int i = index + 1; i < size; i++){
+            data[i-1] = data[i];
+        }
+        size --;
+        return ret;
+    }
+
+    // 删除第一个元素
+    public int removeFirst(){
+        return remove(0);
+    }
+
+    // 删除最后一个元素
+    public int removeLast(){
+        return remove(size - 1);
+    }
+
+    // 删除指定的元素
+    public void removeElement(int e){
+        int index = find(e);
+        if(index != -1){
+            remove(index);
+        }
+    }
 
     @Override
     public String toString(){
--- Array/src/Main.java
+++ Array/src/Main.java
@@ -11,5 +11,11 @@ public class Main {
 
         arr.addFirst(-1);
         System.out.println(arr);
+
+        arr.remove(2);
+        System.out.println(arr);
+
+        arr.removeElement(4);
+        System.out.println(arr);
     }
 }
