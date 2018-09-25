d927b0b73c67fae6a31bd736c5a08a0a8f75ca06
Huanghuang <18778335525@163.com>
2018-09-25 17:02:23 +0800

    使用链表实现队列

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 //        ArrayQueue<Integer> queue = new ArrayQueue<>();
 //        for (int i = 0; i < 10; i++) {
 //            queue.enqueue(i);
@@ -12,15 +12,26 @@ public class Main {
 //            }
 //        }
 
-        LoopQueue<Integer> queue = new LoopQueue<>();
-        for (int i = 0; i < 10; i++) {
-            queue.enqueue(i);
-            System.out.println(queue);
-
+//        LoopQueue<Integer> queue = new LoopQueue<>();
+//        for (int i = 0; i < 10; i++) {
+//            queue.enqueue(i);
+//            System.out.println(queue);
+//
+//            if (i % 3 == 2) {
+//                queue.dequeue();
+//                System.out.println(queue);
+//            }
+//        }
+        LinkedListQueue<Integer> linkedListQueue = new LinkedListQueue<>();
+        for (int i = 0; i < 5; i++) {
+            linkedListQueue.enqueue(i);
+            System.out.println(linkedListQueue);
             if (i % 3 == 2) {
-                queue.dequeue();
-                System.out.println(queue);
+                linkedListQueue.dequeue();
+                System.out.println(linkedListQueue);
             }
+            }
+
+
         }
     }
-}
