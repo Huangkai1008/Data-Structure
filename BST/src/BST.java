7f4e84d0c12f59db0abf5449315f5dab64d2dad4
Huanghuang <18778335525@163.com>
2018-09-26 10:57:27 +0800

    二分搜索树前序排列

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -37,8 +37,8 @@ public class BST<E extends Comparable<E>> {
     // 向以node为根的二分搜索树中插入元素e,递归算法
     // 返回插入新节点后二分搜索树的根
     private Node add(Node node, E e) {
-        if (node == null){
-            size ++;
+        if (node == null) {
+            size++;
             return new Node(e);
         }
 
@@ -51,12 +51,12 @@ public class BST<E extends Comparable<E>> {
     }
 
     // 看二分搜索树中是否包含元素e
-    public boolean contains(E e){
+    public boolean contains(E e) {
         return contains(root, e);
     }
 
     // 看以node为根的二分搜索树中是否包含元素e, 递归算法
-    private boolean contains(Node node, E e){
+    private boolean contains(Node node, E e) {
 
         if (node == null)
             return false;
@@ -67,5 +67,46 @@ public class BST<E extends Comparable<E>> {
             return contains(node.left, e);
         else // e.compareTo(node.e > 0)
             return contains(node.right, e);
+    }
+
+    // 二分搜索树的前序遍历
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    // 前序遍历以node为根的二分搜索树, 递归算法
+    private void preOrder(Node node) {
+        if (node == null)
+            return;
+
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        generateBSTString(root, 0, res);
+        return res.toString();
+    }
+
+    // 生成以node为根节点，深度为depth的描述二叉树的字符串
+    private void generateBSTString(Node node, int depth, StringBuilder res) {
+        if (node == null) {
+            res.append(generateDepthString(depth) + "null\n");
+            return;
+        }
+
+        res.append(generateDepthString(depth) + node.e + "\n");
+        generateBSTString(node.left, depth + 1, res);
+        generateBSTString(node.right, depth + 1, res);
+    }
+
+    private String generateDepthString(int depth) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < depth; i++)
+            res.append("--");
+        return res.toString();
     }
 }
