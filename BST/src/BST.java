1f731d1d037e51e4192efeb238f2abf689a824e4
Huanghuang <18778335525@163.com>
2018-09-26 09:48:42 +0800

    改进二分搜索树

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -1,61 +1,52 @@
 public class BST<E extends Comparable<E>> {
 
     // 节点
-    private class Node{
-       public E e;
-       public Node left, right;
+    private class Node {
+        public E e;
+        public Node left, right;
 
-       public Node(E e){
-           this.e = e;
-           left = null;
-           right = null;
-       }
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
     }
 
     private Node root;
     private int size;
 
     // 构造函数-空
-    public BST(){
+    public BST() {
         root = null;
         size = 0;
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
     // 向二分搜索树中添加新的元素e
-    public void add(E e){
-
-        if (root ==null){
-            root = new Node(e);
-            size ++;
-        }else
-            add(root, e);
+    public void add(E e) {
+        root = add(root, e);
     }
 
     // 向以node为根的二分搜索树中插入元素e,递归算法
-    private void add(Node node, E e) {
-        if (e.equals(node.e))
-            return;
-        else if (e.compareTo(node.e) < 0 && node.left == null) {
-            node.left = new Node(e);
-            size++;
-            return;
-        } else if (e.compareTo(node.e) > 0 && node.right == null) {
-            node.right = new Node(e);
-            size++;
-            return;
+    // 返回插入新节点后二分搜索树的根
+    private Node add(Node node, E e) {
+        if (node == null){
+            size ++;
+            return new Node(e);
         }
 
         if (e.compareTo(node.e) < 0)
-            add(node.left, e);
-        else // e.compareTo(node.e) > 0
-            add(node.right, e);
+            node.left = add(node.left, e);
+        else if (e.compareTo(node.e) > 0)
+            node.right = add(node.right, e);
+
+        return node;
     }
 }
