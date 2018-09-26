551c68e0cd7e90fd6ff21429c19ee9850cf60efb
Huanghuang <18778335525@163.com>
2018-09-26 09:38:04 +0800

    递归实现数组求和

---
+++ LinkedList/src/recursion/Sum.java
@@ -0,0 +1,24 @@
+package recursion;
+
+public class Sum {
+    /**
+     * 递归实现数组求和
+     *
+     * @param arr
+     * @return sum
+     */
+    public static int sum(int[] arr) {
+        return sum(arr, 0);
+    }
+
+    private static int sum(int[] arr, int l) {
+        if (l == arr.length)
+            return 0;
+        return arr[l] + sum(arr, l + 1);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8};
+        System.out.println(sum(nums));
+    }
+}
