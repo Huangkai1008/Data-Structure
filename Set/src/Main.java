f89ca6f01d795f27a2df35327c5ff2def9a3090e
huangkai <18778335525@163.com>
2018-10-18 17:10:39 +0800

    二分搜索树实现集合

--- Set/src/Main.java
+++ Set/src/Main.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> word1 = new ArrayList<>();
+        FileOperation.readFile("pride-and-prejudice.txt", word1);
+
+        System.out.println("Total words: " + word1.size());
+
+        BSTSet<String> set1 = new BSTSet<>();
+        for (String word: word1)
+            set1.add(word);
+
+        System.out.println("Total different words: " + set1.getSize());
     }
 }
