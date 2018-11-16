1abc0b9bbe79c5a72acd70b164c630184cf1eb31
huangkai <18778335525@163.com>
2018-11-16 14:54:21 +0800

    数组实现线段树初步实现

--- Segment-Tree/src/SegmentTree.java
+++ Segment-Tree/src/SegmentTree.java
@@ -5,9 +5,12 @@ public class SegmentTree<E> {
 
     private E[] data;
     private E[] tree;
+    private Merger<E> merger;
 
 
-    public SegmentTree(E[] arr){
+    public SegmentTree(E[] arr, Merger<E> merger){
+        this.merger = merger;
+
         data = (E[])new Object[arr.length];
 
         for (int i = 0; i < arr.length; i++) {
@@ -15,6 +18,26 @@ public class SegmentTree<E> {
         }
 
         tree = (E[]) new Object[4 * arr.length];
+
+        buildSegmentTree(0, 0, data.length - 1);
+    }
+
+
+    // 在treeIndex位置创建[l..r]的线段树
+    private void buildSegmentTree(int treeIndex, int l, int r){
+        if (l == r){
+            tree[treeIndex] = data[l];
+            return;
+        }
+
+        int leftTreeIndex = leftChild(treeIndex);
+
+        int rightTreeIndex = rightChild(treeIndex);
+
+        int mid = l + (r - l) / 2;
+        buildSegmentTree(leftTreeIndex, l, mid);
+
+        buildSegmentTree(rightTreeIndex, mid + 1, r);
     }
 
     public int getSize(){
@@ -35,12 +58,28 @@ public class SegmentTree<E> {
         return 2*index + 1;
     }
 
-    // 返回完全二叉树的数组表示中国，一个索引所代表的右孩子的索引
+    // 返回完全二叉树的数组表示中，一个索引所代表的右孩子的索引
     private int rightChild(int index){
         return 2*(index + 1);
     }
 
 
-
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        res.append('[');
+        for (int i = 0; i < tree.length; i++) {
+            if (tree[i] != null){
+                res.append(tree[i]);
+            }else {
+                res.append("null");
+            }
+            if (i != tree.length - 1){
+                res.append(", ");
+            }
+        }
+        res.append(']');
+        return res.toString();
+    }
 
 }
