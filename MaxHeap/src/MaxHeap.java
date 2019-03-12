b26642d2c28f036e5f41422f5a043d1ade336081
huangkai <18778335525@163.com>
2019-03-12 22:22:50 +0800

    最大堆

---
+++ MaxHeap/src/MaxHeap.java
@@ -0,0 +1,43 @@
+public class MaxHeap<E extends Comparable<E>> {
+    /*
+     * 最大堆
+     */
+    private Array<E> data;
+
+    public MaxHeap(int capacity) {
+        data = new Array<>(capacity);
+    }
+
+
+    public MaxHeap() {
+        data = new Array<>();
+    }
+
+    // 返回堆中的元素个数
+    public int size() {
+        return data.getSize();
+    }
+
+    // 堆中是否为空
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    // 返回完全二叉树的数组表示中，一个索引所表现的元素的父亲节点的索引
+    private int parent(int index) {
+        if (index == 0)
+            throw new IllegalArgumentException("index-0 doesn't have parent");
+        return (index - 1) / 2;
+
+    }
+
+    // 左孩子索引
+    private int leftChild(int index) {
+        return index * 2 + 1;
+    }
+
+    // 右孩子索引
+    private int rightChild(int index) {
+        return index * 2 + 2;
+    }
+}
